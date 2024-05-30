f5322e56c7e00c4c8401253e0260929b8f4b55cd
VivaanKh <159392265+VivaanKh@users.noreply.github.com>
2024-05-30 17:09:53 +1200

    Complete assessment quiz
    Complete assessment quiz with fixed comments.

--- Quiz.py
+++ Quiz.py
@@ -1,4 +1,4 @@
-import random
+import random   # allows random numbers
 
 
 # Checks whether user enters yes (y) or no (n)
@@ -16,7 +16,7 @@ def yes_no(question):
             print("Please enter yes / no.")
 
 
-# instructions
+# instructions function
 def instruction():
     print('''
     **** Instructions ****
@@ -31,40 +31,38 @@ Good Luck & Have Fun!
 
 # operation options for user to select (addition, subtraction, multiplication, division)
 def addition():
-    num_1 = random.randint(0, 100)
-    num_2 = random.randint(0, 100)
-    answer = num_1 + num_2
-    question = f"What is {num_1} + {num_2}? \n"
-    return question, answer
+    num_1 = random.randint(0, 100)  # generates random numbers from 0 - 100
+    num_2 = random.randint(0, 100)  # generates random numbers from 0 - 100
+    answer = num_1 + num_2          # Answer will be first number + second number.
+    question = f"What is {num_1} + {num_2}? \n"    # Creates question that user will answer
+    return question, answer         # returns question and answer for user
 
 
 def subtraction():
-    num_1 = random.randint(0, 100)
-    num_2 = random.randint(0, num_1)
-    answer = num_1 - num_2
-    question = f"What is {num_1} - {num_2}? \n"
-    return question, answer
+    num_1 = random.randint(0, 100)      # generates random numbers from 0 - 100
+    num_2 = random.randint(0, num_1)    # Generates random number between 0 and first number (so answer isn't negative)
+    answer = num_1 - num_2              # answer will be first number minus second number.
+    question = f"What is {num_1} - {num_2}? \n"    # creates question that user will answer
+    return question, answer             # returns question and answer for user
 
 
 def multiplication():
-    num_1 = random.randint(1, 12)
-    num_2 = random.randint(1, 12)
-    answer = num_1 * num_2
-    question = f"What is {num_1} x {num_2}? \n"
-    return question, answer
+    num_1 = random.randint(1, 12)   # generates random numbers from 1 - 12
+    num_2 = random.randint(1, 12)   # generates random numbers from 1 - 12
+    answer = num_1 * num_2          # answer is first number multiplied by second number
+    question = f"What is {num_1} x {num_2}? \n"    # creates question that user will answer
+    return question, answer         # returns question and answer for user
 
 
 def division():
-    num_2 = random.randint(1, 12)
-    answer = random.randint(1, 12)
-    num_1 = num_2 * answer
-    question = f"What is {num_1} ➗ {num_2}? \n"
-    return question, answer
+    num_2 = random.randint(1, 12)   # second number will be a number between 1 and 12
+    answer = random.randint(1, 12)  # answer is a number between 1 and 12
+    num_1 = num_2 * answer  # first number will be second number multiplied by answers number. (stops decimal answers)
+    question = f"What is {num_1} ➗ {num_2}? \n"    # creates question that user will answer
+    return question, answer         # returns question and answer for user
 
 
 # initialising variables
-correct_answers = 0
-incorrect_answers = 0
 questions_answered = 0
 operations = []
 game_history = []
@@ -74,49 +72,51 @@ print()
 print("Welcome to my math quiz!")
 print()
 
-# displays instructions to user
+# Calls instructions function and asks the user if they would like to read the instructions
 want_instructions = yes_no("Do you want to read the instructions? ")
+
+# if user enters "yes" instructions will be displayed to the user
 if want_instructions == "yes":
     instruction()
 
 
-# displays all operation options
+# displays all operation options from their functions for user to select.
 want_addition = yes_no("Would you like to use addition in your quiz? ")
-if want_addition == "yes":
+if want_addition == "yes":  # Calls Addition function if user replies with "yes"
     operations.append(addition)
     print("You will get addition questions.")
     print()
-elif want_addition == "no":
+elif want_addition == "no":    # Ignores addition function if user replies with "no"
     print("Your quiz will not include addition.")
     print()
 
 
 want_subtraction = yes_no("Would you like to use subtraction in your quiz? ")
-if want_subtraction == "yes":
+if want_subtraction == "yes":   # Calls subtraction function if user replies with "yes"
     operations.append(subtraction)
     print("You will get subtraction questions.")
     print()
-elif want_subtraction == "no":
+elif want_subtraction == "no":    # Ignores subtraction function if user replies with "no"
     print("Your quiz will not include subtraction.")
     print()
 
 
 want_multiplication = yes_no("Would you like to use multiplication in your quiz? ")
-if want_multiplication == "yes":
+if want_multiplication == "yes":    # Calls multiplication function if user replies with "yes"
     operations.append(multiplication)
     print("You will get multiplication questions.")
     print()
-elif want_multiplication == "no":
+elif want_multiplication == "no":   # Ignores multiplication function if user replies with "no"
     print("Your quiz will not include multiplication.")
     print()
 
 
 want_division = yes_no("Would you like to use division in your quiz? ")
-if want_division == "yes":
+if want_division == "yes":  # Calls division function if user replies with "yes"
     operations.append(division)
     print("You will get division questions.")
     print()
-elif want_division == "no":
+elif want_division == "no":    # Ignores division function if user replies with "no"
     print("Your quiz will not include division.")
     print()
 
@@ -127,6 +127,7 @@ if not operations:
 # creates loop
 else:
     while True:
+        # asks user how many questions they want to answer
         questions = int(input("How many questions would you like to answer? "))
 
         if questions <= 0:  # user cannot do a quiz without questions, end quiz if user chooses 0 questions.
@@ -152,31 +153,24 @@ else:
                 if user_response == answer:
                     questions_answered += 1
                     print("You got it right! ")
-                    correct_answers += 1
-                    history_item = f" Question: {questions_answered}: Your answer of {answer} was correct!"
+                    history_item = f"Question {questions_answered}: {question} Your answer of {answer} was correct!"
                     game_history.append(history_item)
 
                     # if user didn't get it right, they got it wrong.
                 else:
                     questions_answered += 1
                     print(f"Sorry, that's incorrect, the correct answer was {answer} ")
-                    incorrect_answers += 1
-                    history_item = f" Question: {questions_answered}: Sorry that was incorrect," \
+                    history_item = f"Question {questions_answered}: {question} Sorry, {user_response} was incorrect," \
                                    f" the correct answer was {answer}"
                     game_history.append(history_item)
 
-            # Asks if user would like to see quiz results.
-            results = yes_no("Quiz complete! Would you like to see your results? ")
-            if results == "yes":
-                print(f"You got {correct_answers} questions correct and {incorrect_answers} questions incorrect. ")
-
-            # asks if user wants to see game history and if they do print game history.
+            # asks if user wants to see quiz history and if they do print quiz history.
             if questions_answered > 0:
                 see_history = yes_no("Do you want to see your quiz history? ")
 
-                if see_history == "yes":
-                    print("Game History: ")
+                if see_history == "yes":    # if user responds "yes" print quiz history
+                    print("Quiz History: ")
 
                     for item in game_history:
                         print(item)
-                        
+            break   # ends the loop and quiz
